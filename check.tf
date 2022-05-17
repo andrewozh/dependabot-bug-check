26c031a2df68192aacee795b1e863764255863b4
andrewozh <andrew.ozhegov@gmail.com>
2022-05-17 11:54:02 +0300

    fake terraform manifest

---
+++ check.tf
@@ -0,0 +1,14 @@
+module "first-dep" {
+  source  = "cloudposse/elasticache-redis/aws"
+  version = "0.42.0"
+
+  name = "it supposed to be updated by dependabot"
+}
+
+module "second-dep" {
+  source  = "cloudposse/elasticache-redis/aws"
+  version = "0.42.0"
+
+  name = "it supposed not to be updated by dependabot"
+
+}
