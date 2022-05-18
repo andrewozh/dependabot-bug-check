4373e492a89dabe7327d1417f74b11a1c2008e52
andrewozh <andrew.ozhegov@gmail.com>
2022-05-19 00:42:59 +0300

    two different dependenciaes in a single file

---
+++ check_diffurl.tf
@@ -0,0 +1,14 @@
+module "first-dep" {
+  source  = "cloudposse/elasticache-redis/aws"
+  version = "0.42.0"
+
+  name = "it supposed to be updated by dependabot"
+}
+
+module "second-dep" {
+  source  = "terraform-aws-modules/rds-aurora/aws"
+  version = "6.2.0"
+
+  name = "it supposed not to be updated by dependabot"
+
+}
