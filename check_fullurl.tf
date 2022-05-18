444c589335dbd68f3cd1827362d29b69e249242b
andrewozh <andrew.ozhegov@gmail.com>
2022-05-19 01:09:19 +0300

    full url

---
+++ check_fullurl.tf
@@ -0,0 +1,14 @@
+module "first-dep" {
+  source  = "cloudposse/elasticache-redis/aws"
+  version = "0.42.0"
+
+  name = "it supposed to be updated by dependabot"
+}
+
+module "second-dep" {
+  source  = "github.com/cloudposse/terraform-aws-elasticache-redis"
+  version = "0.42.0"
+
+  name = "it supposed not to be updated by dependabot"
+
+}
