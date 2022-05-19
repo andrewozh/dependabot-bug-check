e2ba204957779c680baa11289b270659407e0687
andrewozh <andrew.ozhegov@gmail.com>
2022-05-19 11:54:27 +0300

    upd

--- check_fullurl.tf
+++ check_fullurl.tf
@@ -6,7 +6,7 @@ module "first-dep" {
 }
 
 module "second-dep" {
-  source  = "github.com/cloudposse/terraform-aws-elasticache-redis"
+  source  = "git@github.com:cloudposse/terraform-aws-elasticache-redis.git"
   version = "0.42.0"
 
   name = "it supposed not to be updated by dependabot"
